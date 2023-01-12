2795bb721d35c4cd93b06c8be66cfb8f59257973
Tam2412 <tamle.dev@gmail.com>
2023-01-12 21:26:39 +0700

    add structured

---
+++ src/constants/pathApiConstant.js
@@ -0,0 +1,5 @@
+const PATH_API = "http://localhost:8820/api/v1/";
+
+const MYDAYPAGE = "myday-page";
+
+export { PATH_API, MYDAYPAGE };
--- src/pages/MyDayPage/MyDayPage.jsx
+++ src/pages/MyDayPage/MyDayPage.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 
-import SideBar from "../components/layout/SideBar";
-import MyDayRoute from "../components/layout/MyDayRoute";
+import SideBar from "../../components/layout/SideBar";
+import MyDayRoute from "../../components/layout/MyDayRoute";
+import AddTask from "../../components/core/AddTask";
+
+import "./style.css";
 
 import { Col, Container, Row } from "react-bootstrap";
 
 const MyDayPage = () => {
   return (
     <Container fluid className="mydaypage">
-      <Row>
+      <Row className="top_content">
         <Col xs={3}>
           <SideBar />
         </Col>
         <Col>
           <MyDayRoute userName="Tâm Lê Văn" />
         </Col>
+      </Row>
+      <Row className="bottom_content">
+        <div className="AddTaskInput_Block">
+          <AddTask />
+        </div>
       </Row>
     </Container>
   );
