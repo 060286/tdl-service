303bdfb58d1e33ff58c9f40be0825d9b61ba37b2
tamlv <tamle.dev@gmail.com>
2023-04-01 21:22:50 +0700

    Tam: Implement update status of subtask

--- src/components/Accordition/Accordition.jsx
+++ src/components/Accordition/Accordition.jsx
@@ -1,16 +1,24 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Box, Grid, Item, List, ListItem, ListItemButton, Radio, TextField } from "@mui/material"
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Box,
+  Grid,
+  Item,
+  List,
+  ListItem,
+  ListItemButton,
+  Radio,
+  TextField,
+} from "@mui/material";
 
-import { makeStyles } from '@mui/styles/';
+import { makeStyles } from "@mui/styles/";
 const useStyle = makeStyles(() => ({
-
   listItem: {
     borderRadius: "16px",
-    padding: "4px"
+    padding: "4px",
   },
   container: {
     marginTop: "16px",
@@ -18,47 +26,54 @@ const useStyle = makeStyles(() => ({
   },
   gridContainer: {
     marginTop: "16px",
-    height: "100%"
+    height: "100%",
   },
   rightContainer: {
-    overflow: "scroll"
+    overflow: "scroll",
   },
   item: {
     height: "100%",
-    overflow: "scroll"
+    overflow: "scroll",
   },
-  todoTitle: { 
-    wordBreak: "break-all"
-  }
-}))
-export default function Accordition({tasks, title,handleTodoIsCompletedChange, onClick}) {
-  const classes = useStyle()  
-  
+  todoTitle: {
+    wordBreak: "break-all",
+  },
+}));
+export default function Accordition({
+  tasks,
+  title,
+  handleTodoIsCompletedChange,
+  onClick,
+}) {
+  const classes = useStyle();
+
   return (
     <>
       <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            id="allTaskUpComming"
-          >
-            <Typography component={"subtitle1"}>{title}</Typography>
-            </AccordionSummary>
-            <AccordionDetails > 
-            <List className={classes.rightContainer}>
-              {tasks?.map(todo => {
-                return (
-                  <ListItemButton className={classes.listItem} onClick={(() => onClick(todo))}>
-                    <Radio
-                      checked={todo.isCompleted}
-                      onChange={(e) => handleTodoIsCompletedChange(todo, e)}
-                    />
-                    <Typography className={classes.todoTitle}>{todo.title}</Typography>
-                  </ListItemButton>
-                )
-              })}
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} id="allTaskUpComming">
+          <Typography component={"subtitle1"}>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List className={classes.rightContainer}>
+            {tasks?.map((todo) => {
+              return (
+                <ListItemButton
+                  className={classes.listItem}
+                  onClick={() => onClick(todo, title)}
+                >
+                  <Radio
+                    checked={todo.isCompleted}
+                    onChange={(e) => handleTodoIsCompletedChange(todo, e)}
+                  />
+                  <Typography className={classes.todoTitle}>
+                    {todo.title}
+                  </Typography>
+                </ListItemButton>
+              );
+            })}
           </List>
-          </AccordionDetails>
-        </Accordion>
+        </AccordionDetails>
+      </Accordion>
     </>
-  )
+  );
 }
--- src/slices/allMyTaskSlice.js
+++ src/slices/allMyTaskSlice.js
@@ -1,11 +1,16 @@
 import { createSlice, createAsyncThunk, current } from "@reduxjs/toolkit";
-import {differenceInDays, format } from "date-fns"
-import { getAllMyTask, getTaskDetail, updateTodoTitle, updateTodoDescription } from "../adapters/allMyTaskAdapter";
+import { differenceInDays, format } from "date-fns";
+import {
+  getAllMyTask,
+  getTaskDetail,
+  updateTodoTitle,
+  updateTodoDescription,
+} from "../adapters/allMyTaskAdapter";
 import { VARIABLE_STATUS } from "../constants/appStatusConstant";
 import INIT_STATE from "./constant";
 const initialState = {
   allTasks: INIT_STATE.allTasks,
-  infoTask: INIT_STATE.infoTask
+  infoTask: INIT_STATE.infoTask,
 };
 
 export const getTaskById = createAsyncThunk(
@@ -34,7 +39,6 @@ export const getAllTask = createAsyncThunk(
     };
   }
 );
-
 
 export const updateTodoTitleSlice = createAsyncThunk(
   "allMyTask/updateTodoTitleSlice",
@@ -67,6 +71,22 @@ const tasksSlice = createSlice({
     updateTaskDetail(state, action) {
       state.infoTask.data = action.payload.data;
     },
+    updateSubTaskStatus(state, action) {
+      const { id, currentTask } = action.payload;
+
+      if (currentTask === "Today") {
+        const data = state.allTasks.data.allTaskToday;
+
+        data.forEach((obj) => {
+          const index = obj.subTasks.findIndex((el) => el.id === id);
+
+          if (index > -1) {
+            console.log(JSON.stringify(obj.subTasks[index]));
+            obj.subTasks[index].isCompleted = !obj.subTasks[index].isCompleted;
+          }
+        });
+      }
+    },
   },
   extraReducers(builder) {
     builder
@@ -80,49 +100,52 @@ const tasksSlice = createSlice({
       .addCase(updateTodoTitleSlice.fulfilled, (state, action) => {
         state.allTasks.status = VARIABLE_STATUS.SUCCEEDED;
         const day = differenceInDays(
-          new Date(format(new Date(action.payload.data.todoDate), 'MM/dd/yyyy')),
-          new Date(format(new Date(), 'MM/dd/yyyy'))
-        )
-        let field; 
-        if (day === 0) field = 'allTaskToday'
-        if (day === 1) field = 'allTaskTomorrow'
-        if (day > 1) field = 'allTaskUpComming'
+          new Date(
+            format(new Date(action.payload.data.todoDate), "MM/dd/yyyy")
+          ),
+          new Date(format(new Date(), "MM/dd/yyyy"))
+        );
+        let field;
+        if (day === 0) field = "allTaskToday";
+        if (day === 1) field = "allTaskTomorrow";
+        if (day > 1) field = "allTaskUpComming";
 
         // TODO: calculate field by todoDate by this function differenceInBusinessDays
         state.allTasks.data = {
           ...state.allTasks.data,
-          [field]: state.allTasks.data[field].map(item => {
-          if (item.id === action.payload.data.id) {
-            return action.payload.data;
-          }
-          return item;
-        })
-        }
+          [field]: state.allTasks.data[field].map((item) => {
+            if (item.id === action.payload.data.id) {
+              return action.payload.data;
+            }
+            return item;
+          }),
+        };
       })
-    .addCase(updateTodoDescriptionSlice.fulfilled, (state, action) => {
-      state.allTasks.status = VARIABLE_STATUS.SUCCEEDED;
-      const day = differenceInDays(
-        new Date(format(new Date(action.payload.data.todoDate), 'MM/dd/yyyy')),
-        new Date(format(new Date(), 'MM/dd/yyyy'))
-      )
-      let field; 
-      if (day === 0) field = 'allTaskToday'
-      if (day === 1) field = 'allTaskTomorrow'
-      if (day > 1) field = 'allTaskUpComming'
-      // TODO: calculate field by todoDate by this function differenceInBusinessDays
-      state.allTasks.data = {
-        ...state.allTasks.data,
-        [field]: current(state.allTasks.data[field]).map(item => {
-          if (item.id === action.payload.data.id) {
-          return action.payload.data;
-        }
-        return item;
-      })
-      }
-    })
+      .addCase(updateTodoDescriptionSlice.fulfilled, (state, action) => {
+        state.allTasks.status = VARIABLE_STATUS.SUCCEEDED;
+        const day = differenceInDays(
+          new Date(
+            format(new Date(action.payload.data.todoDate), "MM/dd/yyyy")
+          ),
+          new Date(format(new Date(), "MM/dd/yyyy"))
+        );
+        let field;
+        if (day === 0) field = "allTaskToday";
+        if (day === 1) field = "allTaskTomorrow";
+        if (day > 1) field = "allTaskUpComming";
+        // TODO: calculate field by todoDate by this function differenceInBusinessDays
+        state.allTasks.data = {
+          ...state.allTasks.data,
+          [field]: current(state.allTasks.data[field]).map((item) => {
+            if (item.id === action.payload.data.id) {
+              return action.payload.data;
+            }
+            return item;
+          }),
+        };
+      });
   },
 });
-
 
 export const selectAllTasks = (state) => {
   return state.allTaskReducer.allTasks;
@@ -133,7 +156,7 @@ export const selectTaskDetail = (state) => {
 };
 
 export const {
-  actions: { updateTaskDetail },
+  actions: { updateTaskDetail, updateSubTaskStatus },
   reducer: allTaskReducer,
 } = tasksSlice;
 
